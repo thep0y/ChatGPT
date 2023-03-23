d3018a23d148f779310e5750055452fbd7b9fde0
thepoy <thepoy@163.com>
2023-03-23 09:50:25 +0800

    支持代码块语法高亮

--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -8,7 +8,10 @@ import '~/styles/ChatPage.scss'
 const { Header, Content } = Layout
 
 const ChatPage: React.FC = () => {
-  const [messages, setMessages] = useState<Message[]>([])
+  const [messages, setMessages] = useState<Message[]>([
+    { content: '使用 rust 写一个冒泡排序，回答中的代码块要声明使用的语言。', role: 'user', time: now() - 10 },
+    { content: '\n\n使用 Rust 写一个冒泡排 序算法示例代码：\n\n```rust\nfn bubble_sort(arr: &mut [i32]) {\n    let n = arr.len();\n    for i in 0..n {\n        for j in 0..n-i-1 {\n            if arr[j] > arr[j+1] {\n                arr.swap(j, j+1);\n            }\n        }\n    }\n}\n\nfn main() {\n    let mut arr = vec![64, 34, 25, 12, 22, 11, 90];\n    bubble_sort(&mut arr);\n    println!("排序后的数组：{:?}", arr);\n}\n```\n\n在上面的代码 中，我们首先定义了一个 `bubble_sort` 函数，它接受一个可变的整型切片作为参数，使用冒泡排序算法对其进行 排序。\n\n具体来说，内层的循环从数组的第一个元素开始，一直到 `n-i-1` 个元素，每次比较相邻两个元素的大 小，如果前面的元素比后面的元素大，就将它们交换位置。这样一次冒泡排序之后，数组的最后一个元素就是当前未排序的最大值。\n\n外层的循环执行 `n` 次，每次执行一次内层循环，可以保证整个数组都被排序完毕。\n\n在 `main` 函数中，我们首先定义一个包含一些无序元素的整型数组 `arr`，然后调用 `bubble_sort` 函数对其进行排序，并输出排序后的结果。', role: 'assistant', time: now() }
+  ])
 
   // invoke('get_models').then(r => {
   //   console.log(r)
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable react/prop-types */
 import React, { useState, memo } from 'react'
 import { Input, Button } from 'antd'
 import ReactMarkdown from 'react-markdown'
+import type { CodeProps } from 'react-markdown/lib/ast-to-react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import '~/styles/ChatBubble.scss'
 
 interface ChatProps {
@@ -8,24 +12,107 @@ interface ChatProps {
   onSendMessage: (message: string) => void
 }
 
-const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = ({ messages }) => (
-  <ol className='list'>
-    {messages.map(({ content, role, time }, i) => {
-      const sent = role === 'user'
+const Message: React.FC<Message> = memo(({ content, role, time }) => {
+  const sent = role === 'user'
 
+  const renderCodeBlock = ({
+    node,
+    inline,
+    className,
+    children,
+    ...props
+  }: CodeProps): React.ReactElement => {
+    const match = /language-(\w+)/.exec(className ?? '')
+
+    if (!(inline ?? false) && match != null) {
       return (
-        <li
-          key={time}
-          className={`shared ${(sent ?? false) ? 'sent' : 'received'}`}
+        <SyntaxHighlighter
+          style={dark}
+          language={match[1]}
+          PreTag="div"
+          {...props}
         >
-          <ReactMarkdown>
-            {content}
-          </ReactMarkdown>
-        </li>
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
       )
-    })}
+    }
+
+    return (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    )
+  }
+
+  return (
+    <li className={`shared ${sent ? 'sent' : 'received'}`}>
+      <ReactMarkdown components={{ code: renderCodeBlock }}>
+        {content}
+      </ReactMarkdown>
+    </li>
+  )
+})
+
+Message.displayName = 'Message'
+
+const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = memo(({
+  messages
+}) => (
+  <ol className="list">
+    {messages.map(({ content, role, time }) => (
+      <Message
+        key={time}
+        content={content}
+        role={role}
+        time={0}
+      />
+    ))}
   </ol>
-)
+))
+
+MessageList.displayName = 'MessageList'
+
+// const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = ({ messages }) => (
+//   <ol className='list'>
+//     {messages.map(({ content, role, time }, i) => {
+//       const sent = role === 'user'
+
+//       return (
+//         <li
+//           key={time}
+//           className={`shared ${(sent ?? false) ? 'sent' : 'received'}`}
+//         >
+//           <ReactMarkdown
+//             components={{
+//               code ({ node, inline, className, children, ...props }) {
+//                 const match = /language-(\w+)/.exec(className ?? '')
+
+//                 return !(inline ?? false) && (match != null)
+//                   ? (
+//                     <SyntaxHighlighter
+//                       style={dark}
+//                       language={match[1]}
+//                       PreTag="div"
+//                       {...props}
+//                     >
+//                       {String(children).replace(/\n$/, '')}
+//                     </SyntaxHighlighter>
+//                     )
+//                   : (
+//                     <code className={className} {...props}>
+//                       {children}
+//                     </code>
+//                     )
+//               }
+//             }}
+//           >
+//             {content}
+//           </ReactMarkdown>
+//         </li>
+//       )
+//     })}
+//   </ol>
+// )
 
 // const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = ({ messages }) => (
 //   <List
@@ -38,7 +125,9 @@ const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = ({ messages }) =
 //   />
 // )
 
-const MessageInput: React.FC<Omit<ChatProps, 'messages'>> = ({ onSendMessage }) => {
+const MessageInput: React.FC<Omit<ChatProps, 'messages'>> = ({
+  onSendMessage
+}) => {
   const [message, setMessage] = useState('')
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -58,11 +147,13 @@ const MessageInput: React.FC<Omit<ChatProps, 'messages'>> = ({ onSendMessage }) 
   )
 }
 
-const Chat: React.FC<ChatProps> = ({ messages, onSendMessage }: ChatProps) => (
+const Chat: React.FC<ChatProps> = memo(({ messages, onSendMessage }: ChatProps) => (
   <div>
     <MessageList messages={messages} />
     <MessageInput onSendMessage={onSendMessage} />
   </div>
-)
+))
 
-export default memo(Chat)
+Chat.displayName = 'Chat'
+
+export default Chat
