ab97d4946c33db0623cc69b3aac4ea7d30a164ea
thepoy <thepoy@163.com>
2023-03-23 15:18:25 +0800

    消息改用回车发送

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -28,59 +28,6 @@ const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = memo(({
 
 MessageList.displayName = 'MessageList'
 
-// const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = ({ messages }) => (
-//   <ol className='list'>
-//     {messages.map(({ content, role, time }, i) => {
-//       const sent = role === 'user'
-
-//       return (
-//         <li
-//           key={time}
-//           className={`shared ${(sent ?? false) ? 'sent' : 'received'}`}
-//         >
-//           <ReactMarkdown
-//             components={{
-//               code ({ node, inline, className, children, ...props }) {
-//                 const match = /language-(\w+)/.exec(className ?? '')
-
-//                 return !(inline ?? false) && (match != null)
-//                   ? (
-//                     <SyntaxHighlighter
-//                       style={dark}
-//                       language={match[1]}
-//                       PreTag="div"
-//                       {...props}
-//                     >
-//                       {String(children).replace(/\n$/, '')}
-//                     </SyntaxHighlighter>
-//                     )
-//                   : (
-//                     <code className={className} {...props}>
-//                       {children}
-//                     </code>
-//                     )
-//               }
-//             }}
-//           >
-//             {content}
-//           </ReactMarkdown>
-//         </li>
-//       )
-//     })}
-//   </ol>
-// )
-
-// const MessageList: React.FC<Omit<ChatProps, 'onSendMessage'>> = ({ messages }) => (
-//   <List
-//     dataSource={messages}
-//     renderItem={(item) => (
-//       <List.Item>
-//         <ChatBubble message={item.content} role={item.role} time={formatTime(item.time)} />
-//       </List.Item>
-//     )}
-//   />
-// )
-
 const MessageInput: React.FC<Omit<ChatProps, 'messages'>> = ({
   onSendMessage
 }) => {
@@ -90,15 +37,14 @@ const MessageInput: React.FC<Omit<ChatProps, 'messages'>> = ({
     setMessage(e.target.value)
   }
 
-  const handleClick = (): void => {
+  const handleEnter = (): void => {
     onSendMessage(message)
     setMessage('')
   }
 
   return (
     <div>
-      <Input value={message} onChange={handleChange} />
-      <Button onClick={handleClick}>发送</Button>
+      <Input value={message} onChange={handleChange} onPressEnter={handleEnter} />
     </div>
   )
 }
