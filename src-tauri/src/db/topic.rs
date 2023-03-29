b8cd7bdee25c3c6a20463cfced700bcb70663cc4
thepoy <thepoy@163.com>
2023-03-29 13:33:40 +0800

    改进代码

--- src-tauri/src/db/topic.rs
+++ src-tauri/src/db/topic.rs
@@ -18,75 +18,66 @@ const TOPIC_INSERT: &str = r#"
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Topic {
-    id: u32,
-    name: String,
-    created_at: u64,
+    pub id: u32,
+    pub name: String,
+    pub created_at: u64,
 }
 
 impl Topic {
-    pub fn new(name: &str, created_at: &u64) -> Self {
-        return Topic {
+    pub fn new(name: &str, created_at: u64) -> Self {
+        Topic {
             id: 0,
-            name: name.into(),
-            created_at: *created_at,
-        };
+            name: name.to_string(),
+            created_at,
+        }
     }
 
-    fn _insert(self, conn: &Connection) -> Result<usize> {
-        conn.execute(TOPIC_INSERT, (self.name, self.created_at))
+    fn insert(&self, conn: &Connection) -> Result<usize> {
+        conn.execute(TOPIC_INSERT, (&self.name, self.created_at))
             .map_err(|e| e.to_string())
     }
 
-    pub fn insert<'a, T: Into<Option<&'a Connection>>>(self, conn: T) -> Result<usize> {
-        match conn.into() {
-            Some(conn) => Self::_insert(self, conn),
-            None => {
-                let conn = new_connection().map_err(|e| e.to_string())?;
-
-                Self::_insert(self, &conn)
-            }
-        }
+    pub fn insert_with_new_conn(&self) -> Result<usize> {
+        let conn = new_connection().map_err(|e| e.to_string())?;
+        self.insert(&conn)
     }
 }
 
 pub fn topic_exists(conn: &Connection, topic_id: u32) -> Result<bool> {
-    let query = "SELECT EXISTS(SELECT 1 FROM topic WHERE topic_id = ? LIMIT 1)";
-    let mut stmt = conn.prepare(query).map_err(|e| e.to_string())?;
-    let exists = stmt
-        .query_row(params![topic_id], |row| row.get(0))
+    let query = "SELECT EXISTS(SELECT 1 FROM topic WHERE id = ? LIMIT 1)";
+    let exists = conn
+        .query_row(query, params![topic_id], |row| row.get(0))
         .map_err(|e| e.to_string())?;
     Ok(exists)
 }
 
 pub fn topic_exists_by_name(conn: &Connection, name: &str) -> Result<bool> {
     let query = "SELECT EXISTS(SELECT 1 FROM topic WHERE name = ? LIMIT 1)";
-    let mut stmt = conn.prepare(query).map_err(|e| e.to_string())?;
-    let exists = stmt
-        .query_row(params![name], |row| row.get(0))
+    let exists = conn
+        .query_row(query, params![name], |row| row.get(0))
         .map_err(|e| e.to_string())?;
     Ok(exists)
 }
 
-pub(crate) fn init_topic() -> Result<()> {
-    let conn = new_connection().map_err(|e| e.to_string())?;
+const FREE_TOPIC_NAME: &str = "自由对话";
+
+pub(crate) fn init_topic(conn: &Connection) -> Result<()> {
     conn.execute(TOPIC_TABLE, ()).map_err(|e| e.to_string())?;
 
-    let free_name = "自由对话";
-
-    if topic_exists_by_name(&conn, free_name)? {
+    if topic_exists_by_name(&conn, FREE_TOPIC_NAME)? {
         debug!("自由对话主题已存在");
         return Ok(());
     }
 
     let free = Topic {
         id: 0,
-        name: free_name.into(),
+        name: FREE_TOPIC_NAME.to_string(),
         created_at: OffsetDateTime::now_local()
             .map_err(|e| e.to_string())?
             .unix_timestamp() as u64,
     };
-    conn.execute(TOPIC_INSERT, (free.name, free.created_at))
-        .map_err(|e| e.to_string())?;
+
+    free.insert(conn)?;
 
     Ok(())
 }
@@ -95,9 +86,9 @@ const SELECT_ALL_TOPICS: &str = r#"
     SELECT id, name, created_at FROM topic
 "#;
 
-fn get_topics(conn: &Connection) -> Result<Vec<Topic>> {
+pub fn get_all_topics(conn: &Connection) -> Result<Vec<Topic>> {
     let mut stmt = conn.prepare(SELECT_ALL_TOPICS).map_err(|e| e.to_string())?;
-    let topics_iter = stmt
+    let topics = stmt
         .query_map([], |row| {
             Ok(Topic {
                 id: row.get(0)?,
@@ -105,37 +96,9 @@ fn get_topics(conn: &Connection) -> Result<Vec<Topic>> {
                 created_at: row.get(2)?,
             })
         })
+        .map_err(|e| e.to_string())?
+        .collect::<Result<Vec<_>, _>>()
         .map_err(|e| e.to_string())?;
 
-    let mut topics = Vec::<Topic>::new();
-    for topic in topics_iter {
-        topics.push(topic.map_err(|e| e.to_string())?);
-    }
-
     Ok(topics)
-}
-
-/// 返回包含所有话题的 Result<Vec<Topic>>
-///
-/// # Params
-///
-/// - `conn`: 一个连接到 chat.db 的引用。
-/// 如果没有传入 conn，则新建一个 conn。
-///
-/// # Returns
-///
-/// 包含所有话题的 Result<Vec<Topic>>
-///
-/// # Error
-///
-/// 返回 Rusqlite 的错误信息
-pub fn get_all_topics<'a, T: Into<Option<&'a Connection>>>(conn: T) -> Result<Vec<Topic>> {
-    match conn.into() {
-        Some(conn) => get_topics(conn),
-        None => {
-            let conn = new_connection().map_err(|e| e.to_string())?;
-
-            get_topics(&conn)
-        }
-    }
 }
