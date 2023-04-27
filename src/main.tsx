dd3b526df8d7bbbce63405a79ec93bdf7d74d477
thepoy <thepoy@163.com>
2023-04-27 16:05:23 +0800

    自动切换明暗主题

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 // import '~/styles/index.scss'
 // import TitleBar from '~/components/TitleBar'
+import { ConfigProvider, theme } from 'antd'
 import '~/styles/index.scss'
+import { appWindow } from '@tauri-apps/api/window'
 
 const ChatPage = lazy(async () => await import('~/components/ChatPage'))
 
@@ -37,9 +39,15 @@ const router = createBrowserRouter([
   }
 ])
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    {/* <TitleBar /> */}
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
+appWindow.theme().then(v => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <ConfigProvider theme={{
+        algorithm: v === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm
+      }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
+    </React.StrictMode>
+  )
+}).catch(() => {})
